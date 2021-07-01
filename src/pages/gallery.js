e0c9f0b642455b42b109fcbfd9e3107d1be6bb05
david69-js <da3909622@gmail.com>
2021-06-30 18:47:05 -0600

    gallery page

--- src/pages/gallery.js
+++ src/pages/gallery.js
@@ -1,11 +1,64 @@
 import React from "react"
 import Layout from "../components/layout"
 import Seo from "../components/seo"
-const Gallery = () => {
+import RecortText from '../components/functions/recortText'
+import { graphql, Link } from "gatsby"
+import BackgroundImage from "gatsby-background-image"
+export const query = graphql`
+query {
+  allPrismicProducts {
+    edges {
+      node {
+        data {
+          slug
+          title_product {
+            text
+          }
+          text_product {
+            text
+          }
+          image_product{
+						localFile{
+              sharp: childImageSharp {
+                fluid(quality: 100) {
+                  ...GatsbyImageSharpFluid_withWebp_noBase64
+                }
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+}
+`
+
+const Gallery = ({ data }) => {
+    const { allPrismicProducts } = data;
+    const prismicProducts = allPrismicProducts.edges.map(({ node }) => node.data)
+    const fullContainerImage = {
+        height: '100%'
+    }
     return (
         <Layout path="/gallery">
             <Seo title="Gallery" />
-            <h2>Gallery</h2>
+            <div className="home-container container w-full max-w-full">
+                <div className="home-container_gallery container mx-auto px-5 pt-4">
+                    {
+                        prismicProducts.map((item, index) => (
+                            <Link to={'/products/' + item.slug} key={index} className={`home-container__products ${index % 2 === 0 ? 'grid-image_small' : 'grid-image_big'}`} >
+                                <div className="h-full">
+                                    <BackgroundImage fluid={item.image_product.localFile.sharp.fluid} class="h-full" style={fullContainerImage} >
+                                    </BackgroundImage>
+                                </div>
+                                <div className="home-container__body h-full w-full flex justify-center items-center">
+                                    <h3 className={`home-container__title text-5xl ${index % 2 === 0 ? 'md:text-4xl' : 'md:text-6xl'}`}>{RecortText(item.title_product.text, 40)}</h3>
+                                </div>
+                            </Link>
+                        ))
+                    }
+                </div>
+            </div>
         </Layout>
     )
 
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,69 +1,41 @@
 import * as React from "react"
-import { graphql, Link } from "gatsby"
+import { graphql } from "gatsby"
 import BackgroundImage from "gatsby-background-image"
 import Layout from "../components/layout"
 import Seo from "../components/seo"
-import RecortText from '../components/functions/recortText'
+
 
 export const query = graphql`
 query {
-  allPrismicProducts {
-    edges {
-      node {
-        data {
-          slug
-          title_product {
-            text
-          }
-          text_product {
-            text
-          }
-          image_product{
-						localFile{
-              sharp: childImageSharp {
+  home: prismicHome {
+    data{
+    home_title {
+      text
+    }
+    home_sub_title {
+      text
+    }
+      home_image {
+        localFile {
+                sharp: childImageSharp {
                 fluid(quality: 100) {
                   ...GatsbyImageSharpFluid_withWebp_noBase64
                 }
               }
             }
           }
-        }
       }
     }
-  }
-home: prismicHome {
-  data{
-   home_title {
-     text
-   }
-   home_sub_title {
-     text
-   }
-     home_image {
-       localFile {
-              sharp: childImageSharp {
-              fluid(quality: 100) {
-                ...GatsbyImageSharpFluid_withWebp_noBase64
-              }
-            }
-          }
-        }
-     }
-   }
 }
 `
 
 const IndexPage = ({ data }) => {
   const {
-    allPrismicProducts,
     home: {
       data: { home_title, home_sub_title, home_image }
     }
   } = data;
-  const prismicProducts = allPrismicProducts.edges.map(({ node }) => node.data)
-  const fullContainerImage = {
-    height: '100%'
-  }
+
   return (
     <Layout path="/">
       <Seo title="Home" />
@@ -81,21 +53,6 @@ const IndexPage = ({ data }) => {
             </div>
           </BackgroundImage>
         }
-        <div className="home-container_gallery container mx-auto px-5 pt-4">
-          {
-            prismicProducts.map((item, index) => (
-              <Link to={'/products/' + item.slug} key={index} className={`home-container__products ${index % 2 === 0 ? 'grid-image_small' : 'grid-image_big'}`} >
-                <div className="h-full">
-                  <BackgroundImage fluid={item.image_product.localFile.sharp.fluid} class="h-full" style={fullContainerImage} >
-                  </BackgroundImage>
-                </div>
-                <div className="home-container__body h-full w-full flex justify-center items-center">
-                  <h3 className={`home-container__title text-5xl ${index % 2 === 0 ? 'md:text-4xl' : 'md:text-6xl'}`}>{RecortText(item.title_product.text, 40)}</h3>
-                </div>
-              </Link>
-            ))
-          }
-        </div>
       </div>
     </Layout>
   )
