549a0e8e066348026f58b8e81e8ff3e38a3890ed
david69-js <da3909622@gmail.com>
2021-07-13 22:00:02 -0600

    text and image

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,14 +1,69 @@
-import React from "react"
-import Layout from "../components/layout"
+import React from "react";
+import Layout from "../components/layout";
 import Seo from "../components/seo"
-const About = () => {
+import { graphql, Link } from "gatsby";
+import { RichText } from "prismic-reactjs";
+import ResponsiveImage from "gatsby-image"
 
-    return (
-        <Layout path="/about">
-            <Seo title="About" />
-            <h2>About</h2>
-        </Layout>
-    )
+export const query = graphql`
+query dataAbout {
+    allPrismicHome {
+      edges {
+        node {
+          data {
+            about_image {
+                localFile{
+                sharp: childImageSharp {
+                  fluid(quality: 100) {
+                    ...GatsbyImageSharpFluid_withWebp_noBase64
+                  }
+                }
+              }
+            }
+            about_sub_text_content {
+              raw
+            }
+            about_sub_title_content {
+              text
+            }
+            about_text_content {
+              raw
+            }
+            about_title {
+              text
+            }
+          }
+        }
+      }
+    }
+  }
+  
+`
+const About = ({ data }) => {
+
+  const {
+    data: { about_title, about_sub_title_content, about_text_content, about_image, about_sub_text_content }
+
+  } = data.allPrismicHome.edges[0].node;
+
+  return (
+    <Layout path="/about">
+      <Seo title="About" />
+      <div className="about w-full max-w-full">
+        <div className="about-container container mx-auto px-4">
+          <h1 className="about-container__title fAlfa-bold text-center">{about_title.text}</h1>
+          <h2 className="about-container__sub-title fAlfa-medium text-center">{about_sub_title_content.text}</h2>
+          <div className="about-container__text-content"><RichText render={about_text_content.raw} /></div>
+          <ResponsiveImage
+            className="about-container__image"
+            fluid={about_image.localFile.sharp.fluid}
+            loading="lazy"
+            fadeIn={true} />
+          <div className="about-container__text-content"><RichText render={about_sub_text_content.raw} /></div>
+        </div>
+      </div>
+    </Layout>
+  )
 
 }
 
--- src/pages/gallery.js
+++ src/pages/gallery.js
@@ -34,33 +34,33 @@ query {
 `
 
 const Gallery = ({ data }) => {
-    const { allPrismicProducts } = data;
-    const prismicProducts = allPrismicProducts.edges.map(({ node }) => node.data)
-    const fullContainerImage = {
-        height: '100%'
-    }
-    return (
-        <Layout path="/gallery">
-            <Seo title="Gallery" />
-            <div className="gallery-container container w-full max-w-full">
-                <div className="gallery-container_gallery container mx-auto px-5 pt-4">
-                    {
-                        prismicProducts.map((item, index) => (
-                            <Link to={'/products/' + item.slug} key={index} className={`gallery-container__products ${index % 2 === 0 ? 'grid-image_small' : 'grid-image_big'}`} >
-                                <div className="h-full">
-                                    <BackgroundImage fluid={item.image_product.localFile.sharp.fluid} class="h-full" style={fullContainerImage} >
-                                    </BackgroundImage>
-                                </div>
-                                <div className="gallery-container__body h-full w-full flex justify-center items-center">
-                                    <h3 className={`gallery-container__title text-5xl ${index % 2 === 0 ? 'md:text-4xl' : 'md:text-6xl'}`}>{RecortText(item.title_product.text, 40)}</h3>
-                                </div>
-                            </Link>
-                        ))
-                    }
+  const { allPrismicProducts } = data;
+  const prismicProducts = allPrismicProducts.edges.map(({ node }) => node.data)
+  const fullContainerImage = {
+    height: '100%'
+  }
+  return (
+    <Layout path="/gallery">
+      <Seo title="Gallery" />
+      <div className="gallery-container container w-full max-w-full">
+        <div className="gallery-container_gallery container mx-auto px-5 pt-4">
+          {
+            prismicProducts.map((item, index) => (
+              <Link to={'/products/' + item.slug} key={index} className={`gallery-container__products ${index % 2 === 0 ? 'grid-image_small' : 'grid-image_big'}`} >
+                <div className="h-full">
+                  <BackgroundImage fluid={item.image_product.localFile.sharp.fluid} class="h-full" style={fullContainerImage} >
+                  </BackgroundImage>
                 </div>
-            </div>
-        </Layout>
-    )
+                <div className="gallery-container__body h-full w-full flex justify-center items-center">
+                  <h3 className={`gallery-container__title text-5xl ${index % 2 === 0 ? 'md:text-4xl' : 'md:text-6xl'}`}>{RecortText(item.title_product.text, 40)}</h3>
+                </div>
+              </Link>
+            ))
+          }
+        </div>
+      </div>
+    </Layout>
+  )
 
 }
 
